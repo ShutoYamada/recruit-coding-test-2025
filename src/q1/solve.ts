55f2dc83b4f97733a0feb6d1b8000b01c255520f
Guen Nhat King <guennhatking@gmail.com>
2025-10-03 17:22:24 +0700

    年齢/レーティングの規則

--- src/q1/solve.ts
+++ src/q1/solve.ts
@@ -159,7 +159,9 @@ const checkRating = (
   rating: Rating,
   hasAdultInSet: boolean
 ): boolean => {
-  // TODO ここを実装
+  if(rating === "G") return true;
+  if(rating === "PG-12" && age === "Child" && !hasAdultInSet) return false;
+  if(rating === "R18+" && !(age === "Adult")) return false;
   return true;
 };
 
