bb1293700713398be74fc65ed296c3eab292568c
Huynh Hung <hhdoremoneleven@gmail.com>
2025-10-03 13:35:00 +0700

    checkRatingのコメントを追加

--- src/q1/solve.ts
+++ src/q1/solve.ts
@@ -158,13 +158,13 @@ const checkRating = (
   hasAdultInSet: boolean
 ): boolean => {
   // TODO ここを実装
-  if (rating === 'G') return true;
+  if (rating === 'G') return true; // 誰でも可
   if (rating === 'PG-12') {
-    if (age === 'Child' && !hasAdultInSet) return false;
+    if (age === 'Child' && !hasAdultInSet) return false; // Child は Adult 同時購入がなければ不可
     return true;
   }
   if (rating === 'R18+') {
-    return age === 'Adult';
+    return age === 'Adult'; // Adult 以外は不可
   }
   return false;
 };
